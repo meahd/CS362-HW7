6e46ad8c669024f9addfdc17bc46b2d8d03884f7
meahd <63009335+meahd@users.noreply.github.com>
2021-03-09 12:04:04 -0800

    Implemented divisible by 15 functionality

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
 #David Meah - CS362 - HW7 - Q1
 
 def number_counter():
-    for list in range(1,6):
+    for list in range(1,100):
         print(list)
 
-number_counter()
+def is15(fizzbuzz):
+    if fizzbuzz % 15 == 0:
+        return 'FizzBuzz'
+
+is15(15)
